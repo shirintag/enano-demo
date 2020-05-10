e7ed6bc1864831fad25f6ed522080bb49d0df7e0
Shirin Taghavi <taghavi.shirin@gmail.com>
2020-05-10 23:18:46 +0200

    Add checkbox and slider, add parameters page

---
+++ src/CheckboxComponent.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import Checkbox from "@material-ui/core/Checkbox";
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  checkBox: {
+    color: "blue",
+    "&$checked": {
+      color: "blue",
+    },
+  },
+  formControl: {
+    margin: theme.spacing(5),
+  },
+}));
+
+export default function CheckboxesGroup() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    speed: true,
+    size: false,
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  const { size, speed } = state;
+  const error = [size, speed].filter((v) => v).length !== 2;
+
+  return (
+    <div className={classes.root}>
+      <FormControl component="fieldset" className={classes.formControl}>
+        <FormLabel component="legend">Pick one of the parameters</FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                className={classes.checkBox}
+                checked={size}
+                onChange={handleChange}
+                name="size"
+              />
+            }
+            label="Size"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={speed} onChange={handleChange} name="speed" />
+            }
+            label="Speed"
+          />
+        </FormGroup>
+        <FormHelperText>Be careful</FormHelperText>
+      </FormControl>
+      <FormControl
+        required
+        error={error}
+        component="fieldset"
+        className={classes.formControl}
+      >
+        <FormHelperText>You can display an error</FormHelperText>
+      </FormControl>
+    </div>
+  );
+}
---
+++ src/DiscreteSlider.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Slider from "@material-ui/core/Slider";
+
+const useStyles = makeStyles({
+  root: {
+    width: 200,
+  },
+});
+
+function valuetext(value) {
+  return `${value}°C`;
+}
+
+function DiscreteSlider() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Typography id="discrete-slider-small-steps" gutterBottom>
+        max performance drop
+      </Typography>
+      <Slider
+        defaultValue={3}
+        getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider-small-steps"
+        step={0.5}
+        marks
+        min={0}
+        max={20}
+        valueLabelDisplay="auto"
+      />
+    </div>
+  );
+}
+
+export default DiscreteSlider;
