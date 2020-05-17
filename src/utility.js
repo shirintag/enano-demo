0d41f554d6542132b892396d9240b92d645321e3
Shirin Taghavi <taghavi.shirin@gmail.com>
2020-05-17 20:08:39 +0200

    Add download function in utility to download a file onclick on download button in chart step

--- src/utility.js
+++ src/utility.js
@@ -8,3 +8,18 @@ export function bytesToSize(bytes) {
 export function calcolateCompression(cr) {
   return 100 / cr;
 }
+
+export function download(filename, model) {
+  var element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:model/plain;charset=utf-8," + encodeURIComponent(model)
+  );
+  element.setAttribute("download", filename);
+
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+  document.body.removeChild(element);
+}
